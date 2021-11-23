a841190f6f3490e0525d6bca539ff491636677b4
Heejin Na <heejin.info@gmail.com>
2021-11-23 15:04:23 +0900

    :hammer: Create javascript files

---
+++ src/js/storage.js
@@ -0,0 +1,135 @@
+const qnaList = [
+  {
+    a: [
+      {
+        answer: "한식",
+        value: 5,
+      },
+      {
+        answer: "중식",
+        value: 6,
+      },
+      {
+        answer: "일식",
+        value: 7,
+      },
+      {
+        answer: "양식",
+        value: 8,
+      },
+    ],
+  },
+  {
+    a: [
+      {
+        answer: "매운 거",
+        value: 20,
+      },
+      {
+        answer: "안 매운 거",
+        value: 50,
+      },
+    ],
+  },
+  {
+    a: [
+      {
+        answer: "국물 있는 거",
+        value: 20,
+      },
+      {
+        answer: "국물 없는 거",
+        value: 20,
+      },
+    ],
+  },
+  {
+    a: [
+      {
+        answer: "밥",
+        value: 20,
+      },
+      {
+        answer: "밥 아닌 거",
+        value: 20,
+      },
+    ],
+  },
+  {
+    a: [
+      {
+        answer: "차가운 거",
+        value: 20,
+      },
+      {
+        answer: "뜨거운 거",
+        value: 20,
+      },
+    ],
+  },
+  {
+    a: [
+      {
+        answer: "고기 위주",
+        value: 20,
+      },
+      {
+        answer: "야채 위주",
+        value: 20,
+      },
+    ],
+  },
+
+
+];
+
+const resultList = [
+  {
+    num: 0,
+    img: "./assets/images/svg/chicken.svg",
+    name: "치킨",
+    desc: "<span>겉바속촉</span> 니아러이ㅏ렁",
+  },
+  {
+    num: 1,
+    img: "./assets/images/svg/pizza.svg",
+    name: "피자",
+    desc:
+  },
+  {
+    num: 2,
+    img: "./assets/images/svg/chicken.svg",
+    name: "치킨",
+    desc:
+  },
+  {
+    num: 3,
+    img: "./assets/images/svg/chicken.svg",
+    name: "치킨",
+    desc:
+  },
+  {
+    num: 4,
+    img: "./assets/images/svg/chicken.svg",
+    name: "치킨",
+    desc:
+  },
+  {
+    num: 5,
+    img: "./assets/images/svg/chicken.svg",
+    name: "치킨",
+    desc:
+  },
+  {
+    num: 6,
+    img: "./assets/images/svg/chicken.svg",
+    name: "치킨",
+    desc:
+  },
+  {
+    num: 7,
+    img: "./assets/images/svg/chicken.svg",
+    name: "치킨",
+    desc:
+  },
+];
---
+++ src/js/view.js
@@ -0,0 +1,8 @@
+import { $, $$ } from "./helpers.js";
+
+const main = $("#main");
+const qna = $("#qna");
+const result = $("#result");
+
+const endPoint = 6;
+const select = [];
