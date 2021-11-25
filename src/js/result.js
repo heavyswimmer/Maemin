774afb814299997881e4ff20249c0679e5cf2fb7
jay <devejayloper@gmail.com>
2021-11-25 10:00:08 +0900

    🔨 Update script for the map UI

--- src/js/result.js
+++ src/js/result.js
@@ -1,10 +1,12 @@
 const hotplace = document.querySelector("#hotplace");
 const resultPage = document.querySelector("#result");
 const mapPage = document.querySelector("#result_map");
+const main = document.querySelector("main");
 
 hotplace.addEventListener("click", function () {
   resultPage.classList.add("hide");
   mapPage.classList.remove("hide");
+  main.classList.add("hide");
 
   // 키워드로 장소를 검색합니다
   searchPlaces();
