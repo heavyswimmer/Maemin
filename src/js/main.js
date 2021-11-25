7d24492d7f6eac6a30d30b3a46b3ec2329e5b6a9
alsth712 <alsth712@naver.com>
2021-11-25 16:12:09 +0900

    :hammer: Add modal event to main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,41 @@
+const modal = document.getElementById("modal")
+const myLocationBtn = document.getElementById("myLocationBtn") // 현재 위치 버튼
+const locationBtn = document.getElementById("locationBtn")     // 위치 이미지 버튼
+
+
+// = modal 창 =
+function modalOn() {
+  modal.style.display = "flex"
+}
+function isModalOn() {
+  return modal.style.display === "flex"
+}
+function modalOff() {
+  modal.style.display = "none"
+}
+
+myLocationBtn.addEventListener("click", e => {
+  modalOn()
+})
+
+locationBtn.addEventListener("click", e => {
+  modalOn()
+})
+
+const closeModal = modal.querySelector(".close-modal")
+closeModal.addEventListener("click", e => {
+  modalOff()
+})
+modal.addEventListener("click", e => {
+  const evTarget = e.target
+  if(evTarget.classList.contains("modal-overlay")) { // modal 바깥부분 클릭 시 modal 닫기
+      modalOff()
+  }
+})
+window.addEventListener("keyup", e => { // esc 키 누르면 modal 없어짐
+  if(isModalOn() && e.key === "Escape") {
+      modalOff()
+  }
+})
+
 
